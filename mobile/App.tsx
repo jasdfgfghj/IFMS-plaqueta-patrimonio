81d3fdeeb52030d438b42ae50ad2b3d21c6d19ed
jasdfgfghj <debora.ribeirodasilva25@gmail.com>
2021-08-12 21:03:10 -0400

    mensagem

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,6 +4,8 @@ import { StatusBar } from 'react-native';
 
 import { Roboto_400Regular, Roboto_500Medium } from'@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import {Poppins_800ExtraBold } from '@expo-google-fonts/poppins';
+import {Poppins_700Bold } from '@expo-google-fonts/poppins';
 
 import Routes from './src/routes';
 
@@ -11,7 +13,9 @@ export default function App() {
   const [fontsLoaded]  = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
-    Ubuntu_700Bold
+    Ubuntu_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_700Bold
   });
 
   if(!fontsLoaded){
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -3,8 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from "./pages/Home";
-import Points from "./pages/Points";
-import Detail from "./pages/Detail";
 
 const AppStack = createStackNavigator();
 
@@ -18,8 +16,6 @@ const Routes = () => {
                 }
             }}>
                 <AppStack.Screen name="Home" component={Home} />
-                <AppStack.Screen name="Points" component={Points} />
-                <AppStack.Screen name="Detail" component={Detail} />
             </AppStack.Navigator>
         </NavigationContainer>
     );
