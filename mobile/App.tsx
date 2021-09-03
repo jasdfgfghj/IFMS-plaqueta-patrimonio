18d8d08096ff6e56566e300378b09322bd155331
Debora <debora.ribeirodasilva25@gmail.com>
2021-09-02 22:55:04 -0400

    atualização

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,6 @@ import { StatusBar } from 'react-native';
 
 import { Roboto_500Medium } from'@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
-import {Poppins_800ExtraBold } from '@expo-google-fonts/poppins';
 import {Poppins_700Bold } from '@expo-google-fonts/poppins';
 
 import Routes from './src/routes';
@@ -13,7 +12,6 @@ export default function App() {
   const [fontsLoaded]  = useFonts({
     Roboto_500Medium,
     Ubuntu_700Bold,
-    Poppins_800ExtraBold,
     Poppins_700Bold
   });
 
